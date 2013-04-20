2ec16d8c10cd0761b325a7642dbbcd00d6ea940c
Pedro Rogério <pinceladasdaweb@hotmail.com>
2013-04-20 20:45:21 -0300

    Update jQuery to version 2

--- contact.php
+++ contact.php
@@ -183,8 +183,8 @@
 
     </div>
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="assets/js/jquery-1.9.1.min.js"><\/script>')</script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/jquery-2.0.0.min.js"><\/script>')</script>
     <script src="assets/js/app.js"></script>
   </body>
 </html>
