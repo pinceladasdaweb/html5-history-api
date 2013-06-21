55bdcacf0496ff9bbe212a4ede6e7c377e8d328c
Pedro Rogerio <pedro.lemes@locaweb.com.br>
2013-06-21 11:56:34 -0300

    Added includes Header/Footer

--- services.php
+++ services.php
@@ -4,106 +4,7 @@
     <meta charset="utf-8">
     <title>Services &raquo; HTML5 History API</title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <style type="text/css">
-      html {
-        overflow-y: scroll;
-        min-height: 100%
-      }
-      body {
-        padding-top: 20px;
-        padding-bottom: 60px;
-      }
-      .container {
-        margin: 0 auto;
-        max-width: 1000px;
-      }
-      .container > hr {
-        margin: 60px 0;
-      }
-      .placeholder {
-        min-height: 533px;
-        position: relative
-      }
-      .placeholder .loading {
-        background: url(assets/img/preloader.gif) no-repeat;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        height: 39px;
-        width: 39px
-      }
-      .jumbotron {
-        margin: 80px 0;
-        text-align: center;
-      }
-      .jumbotron h1 {
-        font-size: 100px;
-        line-height: 1;
-      }
-      .jumbotron .lead {
-        font-size: 24px;
-        line-height: 1.25;
-      }
-      .jumbotron .btn {
-        font-size: 21px;
-        padding: 14px 24px;
-      }
-      .marketing {
-        margin: 60px 0;
-      }
-      .marketing p + h4 {
-        margin-top: 28px;
-      }
-      .navbar .navbar-inner {
-        padding: 0;
-      }
-      .navbar .nav {
-        margin: 0;
-        display: table;
-        width: 100%;
-      }
-      .navbar .nav li {
-        display: table-cell;
-        width: 1%;
-        float: none;
-      }
-      .navbar .nav li a {
-        font-weight: bold;
-        text-align: center;
-        border-left: 1px solid rgba(255,255,255,.75);
-        border-right: 1px solid rgba(0,0,0,.1);
-      }
-      .navbar .nav li:first-child a {
-        border-left: 0;
-        border-radius: 3px 0 0 3px;
-      }
-      .navbar .nav li:last-child a {
-        border-right: 0;
-        border-radius: 0 3px 3px 0;
-      }
-      .featurette-divider {
-        margin: 80px 0;
-      }
-      .featurette {
-        padding-top: 120px;
-        overflow: hidden;
-      }
-      .featurette-image {
-        margin-top: -120px;
-      }
-      .featurette-image.pull-left {
-        margin-right: 40px;
-      }
-      .featurette-image.pull-right {
-        margin-left: 40px;
-      }
-      .featurette-heading {
-        font-size: 50px;
-        font-weight: 300;
-        line-height: 1;
-        letter-spacing: -1px;
-      }
-    </style>
+    <link href="assets/css/style.css" rel="stylesheet">
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="assets/js/html5shiv.js"></script>
@@ -114,23 +15,11 @@
 
     <div class="container">
 
-      <div class="masthead">
-        <h3 class="muted">HTML5 History API</h3>
-        <div class="navbar">
-          <div class="navbar-inner">
-            <div class="container">
-              <ul class="nav">
-                <li><a href="index">Home</a></li>
-                <li><a href="projects">Projects</a></li>
-                <li class="active"><a href="services">Services</a></li>
-                <li><a href="downloads">Downloads</a></li>
-                <li><a href="about">About</a></li>
-                <li><a href="contact">Contact</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+      <?php
+        $page = 'services';
+
+        include('inc/header.php');
+      ?>
 
       <div class="placeholder">
         <div class="jumbotron">
@@ -151,14 +40,7 @@
 
       <hr>
 
-      <div class="footer">
-        <p>&copy; Company <?php echo date('Y'); ?></p>
-      </div>
+      <?php include('inc/footer.php'); ?>
 
-    </div>
-
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="assets/js/jquery-2.0.0.min.js"><\/script>')</script>
-    <script src="assets/js/app.js"></script>
   </body>
 </html>
